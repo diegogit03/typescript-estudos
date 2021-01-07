3aefd043e4abeb773609bc1b65f30b4a8df6e137
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-01-07 19:20:52 -0300

    interfaces in typescript

---
+++ src/033.interfaces/index.ts
@@ -0,0 +1,26 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+export class Pessoa implements TipoPessoa2 {
+  constructor(public nome: string, public sobrenome: string) {}
+
+  public nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Diego', 'Henrique');
+console.log(pessoa.nomeCompleto());
